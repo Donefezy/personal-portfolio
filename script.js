4f67deec3e998fb25c0aa0c72d92b911e0aec4be
Donefezy <72302383+Donefezy@users.noreply.github.com>
2022-12-11 16:21:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let text = "  donefezy@gmail.com  ";
 let result = text.repeat (999);
 
 document.getElementById("marquee").innerHTML = result;
+
+
+var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
